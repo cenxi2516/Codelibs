d46b5d1ef9c8d62d7fb1d605e8fef567a4e16edf
smallsky2516 <smallsky2516@yeah.net>
2023-12-28 18:33:51 +0800

    feat: 优化avoidFnRepeatInvoke

--- frontEnd/optimize/api/index.ts
+++ frontEnd/optimize/api/index.ts
@@ -1,41 +1,15 @@
-interface FnInvokeRecordType<T> {
-  isInvoke: boolean; // 记录接口是否调用
-  lock: boolean; // 接口调用后加锁
-  result: T; // 接口调用后获取的数据
-  awaitTask?: Promise<T>;
-}
+function avoidFnRepeatInvoke < T > (fn: () => T) {
+    let result: T | null = null;
 
-function avoidFnRepeatInvoke<T>(fn: () => T) {
-  const fnInvokeRecord: FnInvokeRecordType<T> = {
-    isInvoke: false,
-    lock: false,
-    result: null as T,
-  };
+    return new Proxy(fn, {
+        async apply(target, thisRef, argList) {
+            if (result) return await result;
 
-  return new Proxy(fn, {
-    async apply(target, thisRef, argList) {
-      const { isInvoke, result, lock, awaitTask } = fnInvokeRecord;
+            const data = (result = Reflect.apply(target, thisRef, argList));
 
-      if (lock) return await awaitTask;
-
-      fnInvokeRecord.lock = true;
-      if (isInvoke && result) return result;
-
-      const data = (fnInvokeRecord.awaitTask = Reflect.apply(
-        target,
-        thisRef,
-        argList
-      ));
-
-      Object.assign(fnInvokeRecord, {
-        isInvoke: true,
-        result: await data,
-        lock: false,
-      });
-
-      return await data;
-    },
-  });
+            return await data;
+        },
+    });
 }
 
 export default avoidFnRepeatInvoke;
